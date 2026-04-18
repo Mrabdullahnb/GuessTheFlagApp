f1ef6873141a6123dad33d0c3f120c46585aa3a2
Mrabdullahnb <iamabdullahnawaz@gmail.com>
2026-04-18 14:48:23 +0500

    Initial Commit

---
+++ GuessTheFlagApp/GuessTheFlagAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagAppApp.swift
+//  GuessTheFlagApp
+//
+//  Created by Abdullah Butt on 18/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
